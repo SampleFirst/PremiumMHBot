1bfdf8835cd2dc583f9fdc0599151718399c3820
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-24 20:43:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,18 +21,23 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        buttons = [[
-                    InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-                ],[
-                    InlineKeyboardButton('♚ Bᴏᴛ Oᴡɴᴇʀ', callback_data="owner_info"),
-                    InlineKeyboardButton('⌬ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK)
-                ],[
-                    InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about'),
-                    InlineKeyboardButton('Iɴʟɪɴᴇ Sᴇᴀʀᴄʜ ☌', switch_inline_query_current_chat='')
-                ],[
-                    InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
-                  ]]
+        buttons = [
+            [
+                InlineKeyboardButton('➕ Add Me To Your Group ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            ],
+            [
+                InlineKeyboardButton('👑 Bot Owner', callback_data="owner_info"),
+                InlineKeyboardButton('🌟 Support Group', url=GRP_LNK)
+            ],
+            [
+                InlineKeyboardButton('❓ Help', callback_data='help'),
+                InlineKeyboardButton('ℹ️ About', callback_data='about'),
+                InlineKeyboardButton('🔎 Inline Search', switch_inline_query_current_chat='')
+            ],
+            [
+                InlineKeyboardButton('📣 Join Updates Channel 📣', url=CHNL_LNK)
+            ]
+        ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
