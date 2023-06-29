20d8326949b144400930d48800b32ba30a98e698
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-29 22:17:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1,3 +1,4 @@
+7# Kanged From @TroJanZheX
 import asyncio
 import re
 import ast
@@ -12,19 +13,20 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 
+from info import (
+    ADMINS, AUTH_CHANNEL, AUTH_USERS, SUPPORT_CHAT_ID, CUSTOM_FILE_CAPTION, MSG_ALRT, PICS, AUTH_GROUPS,
+    P_TTI_SHOW_OFF, GRP_LNK, CHNL_LNK, NOR_IMG, LOG_CHANNEL_PM, SPELL_IMG, MAX_B_TN, IMDB, SINGLE_BUTTON,
+    FILE_CHANNEL, FILE_FORWARD, SPELL_CHECK_REPLY, IMDB_TEMPLATE, NO_RESULTS_MSG, IS_VERIFY, HOW_TO_VERIFY, FORCE_SUB_1, FORCE_SUB_2
+)
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
 from database.filters_mdb import del_all, find_filter, get_filters
 from database.gfilters_mdb import find_gfilter, get_gfilters, del_allg
-from info import (
-    ADMINS, AUTH_CHANNEL, AUTH_USERS, SUPPORT_CHAT_ID, CUSTOM_FILE_CAPTION, MSG_ALRT, PICS, AUTH_GROUPS,
-    P_TTI_SHOW_OFF, GRP_LNK, CHNL_LNK, NOR_IMG, LOG_CHANNEL, SPELL_IMG, MAX_B_TN, IMDB, SINGLE_BUTTON,
-    SPELL_CHECK_REPLY, IMDB_TEMPLATE, NO_RESULTS_MSG, FORCE_SUB_1, FORCE_SUB_2
-)
+
 from Script import script
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
+from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, send_all, check_verification, get_token
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
