f1cde68ab7d71e6a705a2d834e7f3caa3504fff1
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-06-29 19:44:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -203,7 +203,7 @@ async def start(client, message):
                             ]
                         ]
                     )
-            except FloodWait as e:
+                except Exception as e:
                 await asyncio.sleep(e.x)
                 logger.warning(f"Floodwait of {e.x} sec.")
                 await client.send_cached_media(
@@ -222,7 +222,7 @@ async def start(client, message):
                             ]
                         ]
                     )
-            except Exception as e:
+                except Exception as e:
                 logger.warning(e, exc_info=True)
                 continue
             await asyncio.sleep(1) 
