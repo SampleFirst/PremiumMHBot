6a358218feb88bb931e43118665746b66ab4dcff
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-07-01 18:20:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,209 @@
+<p align="center">
+  <img src="https://telegra.ph/file/796aaae0ba96f9ea7f72e.jpg" alt="PremiumMHUpdate Logo">
+</p>
+<h1 align="center">
+  PremiumMHbot
+</h1>
+
+![Typing SVG](https://readme-typing-svg.herokuapp.com/?lines=Welcome+To+PremiumMHbot!;Created+by+Sample+First!;A+Simple+and+Powerful+Bot!;Index+Files+above+2GB!;A+Bot+with+Double+Buttons!;Start+Message+With+Pic!;And+More+Features!)
+</p>
+
+<a href="https://github.com/SampleFirst/PremiumMHbot/stargazers"><img src="https://img.shields.io/github/stars/SampleFirst/PremiumMHbot?color=black&logo=github&logoColor=black&style=for-the-badge" alt="Stars" /></a>
+<a href="https://github.com/SampleFirst/PremiumMHbot/network/members"> <img src="https://img.shields.io/github/forks/SampleFirst/PremiumMHbot?color=black&logo=github&logoColor=black&style=for-the-badge" /></a>
+<a href="https://github.com/SampleFirst/PremiumMHbot"> <img src="https://img.shields.io/github/repo-size/SampleFirst/PremiumMHbot?color=skyblue&logo=github&logoColor=blue&style=for-the-badge" /></a>   
+<a href="https://github.com/SampleFirst/PremiumMHbot/commits/kdbotz"> <img src="https://img.shields.io/github/last-commit/Joelkb/DQ-The-File-Donor?color=black&logo=github&logoColor=black&style=for-the-badge" /></a>
+<a href="https://github.com/SampleFirst/PremiumMHbot"> <img src="https://img.shields.io/github/contributors/SampleFirst/PremiumMHbot?color=skyblue&logo=github&logoColor=blue&style=for-the-badge" /></a>
+<a href="https://github.com/SampleFirst/PremiumMHbot/blob/master/LICENSE"> <img src="https://img.shields.io/badge/License- GPL 2.0 license -blueviolet?style=for-the-badge" alt="License" /> </a>
+<a href="https://www.python.org/"> <img src="https://img.shields.io/badge/Written%20in-Python-skyblue?style=for-the-badge&logo=python" alt="Python" /> </a>
+<a href="https://pypi.org/project/Pyrogram/"> <img src="https://img.shields.io/pypi/v/pyrogram?color=white&label=pyrogram&logo=python&logoColor=blue&style=for-the-badge" /></a>
+
+## 𝐹𝑒𝑎𝑡𝑢𝑟𝑒𝑠
+
+- ✅ **IMDB Template Set**
+- ✅ **Indexes Files above 2GB**
+- ✅ **PreDVD and CamRip Delete Mode**
+- ✅ **Multiple File Deletion**
+- ✅ **Settings Menu**
+- ✅ **Force Subscription**
+- ✅ **Welcome Message**
+- ✅ **Automatic File Filtering**
+- ✅ **Double Filter Button**
+- ✅ **Single Filter Button**
+- ✅ **Bot PM Send Mode**
+- ✅ **Auto File Send**
+- ✅ **Forward Restriction**
+- ✅ **File Protection**
+- ✅ **Global File Filtering**
+- ✅ **IMDB**
+- ✅ **Admin Commands**
+- ✅ **User Broadcast**
+- ✅ **Group Broadcast**
+- ✅ **Index**
+- ✅ **Admin Search**
+- ✅ **Random Pics**
+- ✅ **IDS and User Info**
+- ✅ **Stats**
+- ✅ **Users**
+- ✅ **Chats**
+- ✅ **User Ban**
+- ✅ **User Unban**
+- ✅ **Chat Leave**
+- ✅ **Chat Disable**
+- ✅ **Channel**
+- ✅ **Channel Disable**
+- ✅ **Channel**
+- ✅ **Spelling Check Feature**
+- ✅ **File Store**
+- ✅ **Auto Delete**
+
+<b>To Know About All Features, Join Our <a href='https://telegram.me/PremiumMHUpdate'>Update Channel</a>.</b>
+
+ ## Commands
+```
+• /invite - 📩 Get the invite link of any chat which the bot is admin.
+• /logs - 📜 Get the recent errors logs.
+• /stats - 📊 Get stats of files in DB.
+• /connections - 🔗 See all connected groups.
+• /settings - ⚙️ Open settings menu.
+• /filter - ➕ Add manual filters.
+• /filters - 👀 View filters.
+• /connect - 🤝 Connect to PM.
+• /disconnect - 🙅 Disconnect from PM.
+• /del - 🗑️ Delete a filter.
+• /delall - 🗑️ Delete all filters.
+• /deleteall - 🗑️ Delete all indexed files.
+• /delete - 🗑️ Delete a specific file from index.
+• /info - ℹ️ Get user info.
+• /id - 🆔 Get user's Telegram ID.
+• /imdb - 🎥 Fetch info from IMDb.
+• /search - 🔎 Search from various sources.
+• /start - ▶️ Start the bot.
+• /setskip - ⏭️ Skip number of messages when indexing files.
+• /users - 👥 Get list of my users and IDs.
+• /chats - 📃 Get list of my chats and IDs.
+• /leave - 🚪 Leave from a chat.
+• /disable - 🚫 Disable a chat.
+• /enable - ✅ Re-enable a chat.
+• /ban - 🚫 Ban a user.
+• /unban - 🆓 Unban a user.
+• /channel - 📢 Get list of total connected channels.
+• /broadcast - 📣 Broadcast a message to all users.
+• /grp_broadcast - 📢 Broadcast a message to all connected groups.
+• /batch - 📑 Create link for multiple posts.
+• /link - 🔗 Create link for one post.
+• /status - 📈 Check your API Key to check dyno, bot uptime, and bot working day prediction.
+• /set_template - 📑 Set a custom IMDB template for individual groups.
+• /gfilter - ➕ Add global filters.
+• /gfilters - 👀 View all global filters.
+• /delg - 🗑️ Delete a specific global filter.
+• /delallg - 🗑️ Delete all global filters from the bot's database.
+• /deletefiles - 🗑️ Delete PReDVDRip and CamRip Files from the bot's database.
+```
+
+## Variables
+
+### Required Variables
+* `BOT_TOKEN`: Create a bot using [@BotFather](https://telegram.dog/BotFather), and get the Telegram API token.
+* `API_ID`: Get this value from [telegram.org](https://my.telegram.org/apps)
+* `API_HASH`: Get this value from [telegram.org](https://my.telegram.org/apps)
+* `CHANNELS`: Username or ID of channel or group. Separate multiple IDs by space
+* `ADMINS`: Username or ID of Admin. Separate multiple Admins by space
+* `DATABASE_URI`: [mongoDB](https://www.mongodb.com) URI. Get this value from [mongoDB](https://www.mongodb.com). For more help watch this [video](https://youtu.be/1G1XwEOnxxo)
+* `DATABASE_NAME`: Name of the database in [mongoDB](https://www.mongodb.com). For more help watch this [video](https://youtu.be/1G1XwEOnxxo)
+* `LOG_CHANNEL` : A channel to log the activities of bot. Make sure bot is an admin in the channel.
+### Optional Variables
+* `PICS`: Telegraph links of images to show in start message.( Multiple images can be used separated by space )
+* `FILE_STORE_CHANNEL`: Channel from were file store links of posts should be made.Separate multiple IDs by space
+* Check [info.py](https://github.com/Joelkb/DQ-The-File-Donor/blob/master/info.py) for more optional variables
+
+
+<details><summary>Deploy To Heroku</summary>
+<p>
+<br>
+<a href="https://heroku.com/deploy?template=https://github.com/Joelkb/DQ-the-file-donor">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy To Heroku">
+</a>
+</p>
+</details>
+<details><summary>Deploy To Heroku Via Bot</summary>
+<p>
+<br>
+<a href="https://telegram.dog/XTZ_HerokuBot?start=Sm9lbGtiL0RRLXRoZS1maWxlLWRvbm9yIG1hc3Rlcg">
+  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy Via Heroku Bot">
+</a>
+</p>
+</details>
+
+<details><summary>Deploy To Koyeb</summary>
+<br>
+<b>The fastest way to deploy the application is to click the Deploy to Koyeb button below.</b>
+<br>
+<br>
+
+[![Deploy to Koyeb](https://www.koyeb.com/static/images/deploy/button.svg)](https://app.koyeb.com/deploy?type=git&repository=github.com/Joelkb/DQ-The-FIle-Donor&branch=web&name=dqthefiledonor)
+</details>
+
+<details><summary>Deploy To Render</summary>
+<br>
+<b>
+Use these commands:
+<br>
+<br>
+• Build Command: <code>pip3 install -U -r requirements.txt</code>
+<br>
+<br>
+• Start Command: <code>python3 bot.py</code>
+<br>
+<br>
+Go to https://uptimerobot.com/ and add a monitor to keep your bot alive.
+<br>
+<br>
+Use these settings when adding a monitor:</b>
+<br>
+<br>
+<img src="https://telegra.ph/file/a79a156e44f43c9833b50.jpg" alt="render template">
+<br>
+<br>
+<b>Click on the below button to deploy directly to render ↓</b>
+<br>
+<br>
+<a href="https://render.com/deploy?repo=https://github.com/Joelkb/DQ-the-file-donor/tree/web">
+<img src="https://render.com/images/deploy-to-render-button.svg" alt="Deploy to Render">
+</a>
+</details>
+
+<details><summary>Deploy To VPS</summary>
+<p>
+<pre>
+git clone https://github.com/Joelkb/DQ-The-File-Donor
+# Install Packages
+pip3 install -U -r requirements.txt
+Edit info.py with variables as given below then run bot
+python3 bot.py
+</pre>
+</p>
+</details>
+
+<hr>
+
+## Credits 
+
+* [![EvaMaria-Devs](https://img.shields.io/static/v1?label=EvaMaria&message=devs&color=critical)](https://telegram.dog/EvaMariaDevs)
+* [![Joel-TGX](https://img.shields.io/static/v1?label=Jᴏᴇʟ+ᠰ+TɢX&message=Github&color=critical)](https://github.com/Joelkb)
+* [![Contact](https://img.shields.io/static/v1?label=Contact+Developer&message=On+Telegram&color=critical)](https://telegram.me/creatorbeatz)
+
+
+## Note 🏷️
+ - <b>Importing this repo instead of forking is strictly prohibited 🚫 Kindly fork and edit as your wish (Must Give Credits for devs) 🙃</b>
+ - <b>If you find any bugs or errors, report it to the developer.</b>
+* [![Contact Developer](https://img.shields.io/static/v1?label=Contact+Developer&message=On+Telegram&color=critical)](https://telegram.me/creatorbeatz)
+
+
+Kanging this codes and and editing a few lines and releasing a V.x  or an [alpha](https://telegram.dog/subin_works/204), beta , gama branches of your repo won't make you a Developer.
+Fork the repo and edit as per your needs.
+
+## Disclaimer
+[![GNU Affero General Public License 2.0](https://www.gnu.org/graphics/agplv3-155x51.png)](https://www.gnu.org/licenses/agpl-3.0.en.html#header)    
+Licensed under [GNU AGPL 2.0.](https://github.com/EvamariaTG/evamaria/blob/master/LICENSE)
+Selling The Codes To Other People For Money Is *Strictly Prohibited*.
+
---
+++ Script.py
@@ -0,0 +1,281 @@
+class script(object):
+    START_TXT = """<b>Hello 👋 {},
+My Name Is <a href=https://t.me/{}>{}</a>, I Can Provide Movies, Just Add Me To Your Group And Enjoy</b>"""
+
+    HELP_TXT = """<b>Hey {}
+Here Is The Help For My Commands.</b>"""
+
+    ABOUT_TXT = """<b>✯ My Name: {}
+✯ Creator: <a href='https://t.me/iPepkornBots'>iPepkorn Bots</a>
+✯ Library: <a href='https://docs.pyrogram.org/'>Pyrogram</a>
+✯ Language: <a href='https://www.python.org/download/releases/3.0/'>Python 3</a>
+✯ Database: <a href='https://www.mongodb.com/'>MongoDB</a>
+✯ Bot Server: <a href='https://app.koyeb.com/'>Koyeb</a>
+✯ Build Status: v1.1.1 [Stable]</b>"""
+
+    SOURCE_TXT = """<b>Note:
+- This bot is not an open-source project.
+
+Developer:
+- <a href="https://t.me/iPepkornBots">iPepkorn Bots</a></b>"""
+
+    MANUELFILTER_TXT = """Help: <b>Filters</b>
+- Filter is a feature where users can set automated replies for a particular keyword and I will respond whenever a keyword is found in the message.
+<b>Note:</b>
+1. This bot should have admin privilege.
+2. Only admins can add filters in a chat.
+3. Alert buttons have a limit of 64 characters.
+
+Commands And Usage:
+• /filter - <code>Add a filter in a chat</code>
+• /filters - <code>List all the filters of a chat</code>
+• /del - <code>Delete a specific filter in a chat</code>
+• /delall - <code>Delete the whole filters in a chat (chat owner only)</code>"""
+
+    BUTTON_TXT = """Help: <b>Buttons</b>
+- This bot supports both URL and alert inline buttons.
+<b>Note:</b>
+1. Telegram will not allow you to send buttons without any content, so content is mandatory.
+2. This bot supports buttons with any Telegram media type.
+3. Buttons should be properly parsed as Markdown format.
+
+URL Buttons:
+<code>[Button Text](buttonurl:https://t.me/PremiumMHUpdate)</code>
+
+Alert Buttons:
+<code>[Button Text](buttonalert:This is an alert message)</code>"""
+
+    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+<b>Note: File Index</b>
+1. Make me the admin of your channel if it's private.
+2. Make sure that your channel does not contain camsrips, porn, and fake files.
+3. Forward the last message to me with quotes. I'll add all the files in that channel to my DB.
+
+<b>Note: AutoFilter</b>
+1. Add the bot as admin on your group.
+2. Use /connect and connect your group to the bot.
+3. Use /settings on bot's PM and turn on AutoFilter on the settings menu."""    
+
+    CONNECTION_TXT = """Help: <b>Connections</b>
+- Used to connect the bot to PM for managing filters.
+- It helps to avoid spamming in groups.
+<b>Note:</b>
+1. Only admins can add a connection.
+2. Send <code>/connect</code> to connect me to your PM.
+
+Commands And Usage:
+• /connect - <code>Connect a particular chat to your PM</code>
+• /disconnect - <code>Disconnect from a chat</code>
+• /connections - <code>List all your connections</code>"""
+
+    EXTRAMOD_TXT = """Help: Extra Modules
+<b>Note:</b>
+These are the extra features of this bot.
+
+Commands And Usage:
+• /id - <code>Get ID of a specified user.</code>
+• /info - <code>Get information about a user.</code>
+• /imdb - <code>Get the film information from IMDb source.</code>
+• /search - <code>Get the film information from various sources.</code>"""
+
+    ADMIN_TXT = """Help: Admin Mods
+<b>Note:</b>
+This module only works for my admins.
+
+Commands And Usage:
+• /logs - <code>To get the recent errors</code>
+• /stats - <code>To get the status of files in DB. [This command can be used by anyone]</code>
+• /delete - <code>To delete a specific file from DB.</code>
+• /users - <code>To get list of my users and IDs.</code>
+• /chats - <code>To get list of my chats and IDs</code>
+• /leave - <code>To leave from a chat.</code>
+• /disable - <code>To disable a chat.</code>
+• /ban - <code>To ban a user.</code>
+• /unban - <code>To unban a user.</code>
+• /channel - <code>To get list of total connected channels</code>
+• /broadcast - <code>To broadcast a message to all users</code>
+• /grp_broadcast - <code>To broadcast a message to all connected groups.</code>
+• /gfilter - <code>To add global filters</code>
+• /gfilters - <code>To view list of all global filters</code>
+• /delg - <code>To delete a specific global filter</code>
+• /request - <code>To send a Movie/Series request to bot admins. Only works on support group. [This command can be used by anyone]</code>
+• /delallg - <code>To delete all Gfilters from the bot's database.</code>
+• /deletefiles - <code>To delete CamRip and PreDVD files from the bot's database.</code>"""
+
+    STATUS_TXT = """<b>★ Total Files: <code>{}</code>
+★ Total Users: <code>{}</code>
+★ Total Chats: <code>{}</code>
+★ Used Storage: <code>{}</code>
+★ Free Storage: <code>{}</code></b>"""
+
+    LOG_TEXT_G = """🆕 #NewGroup
+Group: {} (<code>{}</code>)
+Total Members: <code>{}</code>
+Date: <code>{}</code>
+Time: <code>{}</code>
+Added By: {}
+@PremiumMHBot"""
+
+    LOG_TEXT_P = """🆕 #NewUser
+ID: <code>{}</code>
+Name: {}
+Total: {}
+Date: <code>{}</code>
+Time: <code>{}</code>
+@PremiumMHBot"""
+
+    LOG_TEXT_V = """🆕 #UserVerify
+ID: <code>{}</code>
+Name: {}
+Date: <code>{}</code>
+Time: <code>{}</code>"""
+
+    ALRT_TXT = """Hello {},
+This is not your movie request,
+Request yours..."""
+
+    OLD_ALRT_TXT = """Hey {},
+You are using one of my old messages,
+Please send the request again."""
+
+    CUDNT_FND = """I couldn't find anything related to {}
+Did you mean any one of these?"""
+
+    I_CUDNT = """<b>Sorry, no files were found for your request {} 😕
+
+Check your spelling in Google and try again 😃
+
+Movie request format 👇
+
+Example: Uncharted or Uncharted 2022 or Uncharted English
+
+Series request format 👇
+
+Example: Loki S01 or Loki S01E04 or Lucifer S03E24
+
+🚯 Don't use ➠ ':(!,./)</b> 😕🙅‍♀️🚫"""
+
+    I_CUD_NT = """I couldn't find any movie related to {}. 
+Please check the spelling on Google or IMDb..."""
+
+    MVE_NT_FND = """🤷‍♂️ Movie not found in database..."""
+    
+    TOP_ALRT_MSG = """🔍 Checking for Movie in Database..."""
+    
+    MELCOW_ENG = """<b>Hello {}, and Welcome to {} Group </b> 🌟"""
+    
+    MORE_BOTS = """
+<b>⍟───[ More Bots ]───⍟
+    
+• My name: <a href="https://t.me/PremiumMHBot?start">PremiumMHBot</a> 🤖
+• Bot Two: <a href="https://t.me/Movies_Hole_Robot?start">Movies Hole Bot</a> 🎥
+• Bot Three: <a href="https://t.me/iPopkonBot?start">iPapkornBot</a> 🍿
+• Bot Four: <a href="https://t.me/HDCinemasBot?start">HDCinemasBot</a> 🎬
+• Bot Fifth: <a href="https://t.me/TrueDealsMasterBot?start">TrueDealsMasterBot</a> 💰</b>"""
+
+    
+    REQINFO = """
+⚠️ 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 ⚠️
+
+After 10 minutes, this message will be automatically deleted.
+
+If you do not see the requested movie/series file, look at the next page"""
+
+    MINFO = """
+𝐌𝐨𝐯𝐢𝐞 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭
+
+Go to Google 
+➠ Type movie name 
+➠ Copy correct name 
+➠ Paste this group
+
+Example: Uncharted
+
+🚫 Don't use ➠ ':(!,./)"""
+
+    SINFO = """
+𝐒𝐞𝐫𝐢𝐞𝐬 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐅𝐨𝐫𝐦𝐚𝐭
+
+Go to Google 
+➠ Type series name 
+➠ Copy correct name 
+➠ Paste this group
+
+Example: Loki S01E01
+
+🚫 Don't use ➠ ':(!,./)"""
+
+    NORSLTS = """#NoResults
+ID: <b>{}</b>
+Name: <b>{}</b>
+Message: <b>{}</b>"""
+
+    CAPTION = """<b>📂 Filename: {file_name}
+</b>"""
+
+    FILE_MSG = """
+<b>👋 Hai {} </b>
+
+<b>📫 Your File is Ready</b>
+
+<b>📂 File Name</b> : <code>{}</code>              
+                       
+<b>⚙️ File Size</b> : <b>{}</b>
+"""
+
+    CHANNEL_CAP = """
+<b>👋 Hai {}</b>
+
+<code>{}</code>
+
+⚠️ <b>This file will be deleted from here within 10 minutes as it has copyright... !!!</b>
+
+<b>© Powered by {}</b>
+"""
+    
+    IMDB_TEMPLATE_TXT = """
+<b>Query: {query}
+IMDb Data:
+
+🏷 Title: <a href={url}>{title}</a>
+🎭 Genres: {genres}
+📆 Year: <a href={url}/releaseinfo>{year}</a>
+🌟 Rating: <a href={url}/ratings>{rating}</a> / 10</b>"""
+    
+    ALL_FILTERS = """
+<b>👋 Hey {}, These are my three types of filters. ✨</b>"""
+    
+    GFILTER_TXT = """
+<b>👋 Welcome to Global Filters. Global Filters are the filters set by bot admins which will work on all groups. 🌍</b>
+    
+Available commands:
+• /gfilter - <code>To create a global filter.</code>
+• /gfilters - <code>To view all global filters.</code>
+• /delg - <code>To delete a particular global filter.</code>
+• /delallg - <code>To delete all global filters.</code>"""
+    
+    FILE_STORE_TXT = """
+<b>📂 File store is the feature which will create a shareable link of a single or multiple files. 📦</b>
+
+Available commands:
+• /batch - <code>To create a batch link of multiple files. 📚</code>
+• /link - <code>To create a single file store link. 🔗</code>
+• /pbatch - <code>Just like /batch, but the files will be sent with forward restrictions. 🚫</code>
+• /plink - <code>Just like /link, but the file will be sent with forward restrictions. 🚫🔗</code>"""
+
+RESTART_TXT = """
+<b>🔄 Bot Restarted!
+
+📅 Date: <code>{}</code>
+⏰ Time: <code>{}</code>
+🌐 Timezone: <code>Asia/Kolkata</code>
+🛠️ Build Status: <code>v1.0.0 [Stable]</code></b>"""
+
+    LOGO = """
+
+██████╗░░██████╗░░░░░░░████████╗██╗░░██╗███████╗░░░░░░███████╗██╗██╗░░░░░███████╗░░░░░░██████╗░░█████╗░███╗░░██╗░█████╗░██████╗░
+██╔══██╗██╔═══██╗░░░░░░╚══██╔══╝██║░░██║██╔════╝░░░░░░██╔════╝██║██║░░░░░██╔════╝░░░░░░██╔══██╗██╔══██╗████╗░██║██╔══██╗██╔══██╗
+██║░░██║██║██╗██║█████╗░░░██║░░░███████║█████╗░░█████╗█████╗░░██║██║░░░░░█████╗░░█████╗██║░░██║██║░░██║██╔██╗██║██║░░██║██████╔╝
+██║░░██║╚██████╔╝╚════╝░░░██║░░░██╔══██║██╔══╝░░╚════╝██╔══╝░░██║██║░░░░░██╔══╝░░╚════╝██║░░██║██║░░██║██║╚████║██║░░██║██╔══██╗
+██████╔╝░╚═██╔═╝░░░░░░░░░░██║░░░██║░░██║███████╗░░░░░░██║░░░░░██║███████╗███████╗░░░░░░██████╔╝╚█████╔╝██║░╚███║╚█████╔╝██║░░██║
+╚═════╝░░░░╚═╝░░░░░░░░░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝░░░░░░╚═╝░░░░░╚═╝╚══════╝╚══════╝░░░░░░╚═════╝░░╚════╝░╚═╝░░╚══╝░╚════╝░╚═╝░░╚═╝"""
