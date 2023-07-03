01bc78a3c451b8520aaefb38034486db48069f64
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-07-03 19:08:49 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -158,21 +158,20 @@ class Database:
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
     
-    async def daily_chats_count(today, end):
+    async def daily_chats_count(self, today, end):
         today_date = datetime.strptime(today, '%d %B, %Y').date()
         start = datetime.combine(today_date, datetime.min.time())
         end = datetime.combine(today_date, datetime.max.time())
-        count = await db.grp.count_documents({
+        count = await self.grp.count_documents({
             'timestamp': {'$gte': start, '$lt': end}
         })
         return count
 
-
-    async def daily_users_count(today, end):
+    async def daily_users_count(self, today, end):
         today_date = datetime.strptime(today, '%d %B, %Y').date()
         start = datetime.combine(today_date, datetime.min.time())
         end = datetime.combine(today_date, datetime.max.time())
-        count = await db.col.count_documents({
+        count = await self.col.count_documents({
             'timestamp': {'$gte': start, '$lt': end}
         })
         return count
