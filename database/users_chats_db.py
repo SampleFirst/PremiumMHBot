6eafed3acd71f730bd45cb0cb2ca1832561d9641
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-07-04 14:58:59 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -11,6 +11,8 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
+        self.daily_chats = self.db.daily_chats
+        self.daily_users = self.db.daily_users
 
 
     def new_user(self, id, name):
