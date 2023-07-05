18ae44d28fd0c5f5051e0e8054cc427679cd66dd
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-07-06 01:52:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -92,11 +92,11 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         total_users = await db.total_users_count()
         tz = pytz.timezone('Asia/Kolkata')
-            now = datetime.now(tz)
-            time = now.strftime('%I:%M:%S %p')
-            today = now.date()  # Get the current date in the defined time zone
-            daily_chats = await db.daily_chats_count(today)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(
+        now = datetime.now(tz)
+        time = now.strftime('%I:%M:%S %p')
+        today = now.date()  # Get the current date in the defined time zone
+        daily_chats = await db.daily_chats_count(today)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(
             a=message.from_user.id,
             b=message.from_user.mention,
             c=message.from_user.username,
