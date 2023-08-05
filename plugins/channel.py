3ed5d9a9cf5f49c98e6f6af80da158c20b5515dd
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-05 19:04:17 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from info import CHANNELS
+from info import CHANNELS, CHNL_LNK
 from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
@@ -16,3 +16,12 @@ async def media(bot, message):
     media.file_type = file_type
     media.caption = message.caption
     await save_file(media)
+
+    # Write the new file to the disk (modify this part according to your needs)
+    # Example:
+    with open(f"new_files/{media.file_name}", "wb") as f:
+        await media.download(file_name=f.name)
+
+    # Send a log to the UPDATE_CHANNEL about the new added file
+    log_text = f"New file added: {media.file_name}"
+    await bot.send_message(CHNL_LNK, log_text)
