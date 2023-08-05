e0aa17b5cf7d615e9294ad6dad12af99326c4e40
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-05 22:29:58 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,4 +1,3 @@
-import os
 from pyrogram import Client, filters
 from info import CHANNELS, CHNL_LNK
 from database.ia_filterdb import save_file
@@ -7,15 +6,10 @@ media_filter = filters.document | filters.video | filters.audio
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
-    if message.document:
-        media = message.document
-        file_type = "document"
-    elif message.video:
-        media = message.video
-        file_type = "video"
-    elif message.audio:
-        media = message.audio
-        file_type = "audio"
+    for file_type in ("document", "video", "audio"):
+        media = getattr(message, file_type, None)
+        if media is not None:
+            break
     else:
         return
 
@@ -23,14 +17,6 @@ async def media(bot, message):
     media.caption = message.caption
     await save_file(media)
 
-    # Create the 'new_files' directory if it doesn't exist
-    if not os.path.exists("new_files"):
-        os.makedirs("new_files")
-
-    # Write the new file to the disk
-    with open(os.path.join("new_files", media.file_name), "wb") as f:
-        await media.download(file_name=f.name)
-
     # Send a log to the UPDATE_CHANNEL about the new added file
-    log_text = f"New file added: {media.file_name}"
-    await bot.send_message(CHNL_LNK, log_text)
+    log_message = f"New file added: {media.file_type} - {media.file_name} in {CHANNELS}"
+    await bot.send_message(CHNL_LNK, log_message)
