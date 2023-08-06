795bc98163253888e0c63506ae788819fb84e0f2
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-06 12:58:20 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -23,61 +23,64 @@ async def media(bot, message):
     await save_file(media)
 
     # Extracting the search query from the file name
-    search_query = media.file_name.split(' (')[0]  # Using the part before the first ' (' as the search query
+    file_name = media.file_name.lower()
+    search_query = file_name.split(' (')[0]  # Using the part before the first ' (' as the search query
 
-    # Get the IMDB data and poster based on the search query
-    imdb = await get_poster(search_query)
+    
+        imdb = await get_poster(search_query)
 
-    # Send log in UPDATE_CHANNEL with IMDB_TEMPLATE and IMDB poster
-    if imdb:
-        buttons = [
-            [
-                InlineKeyboardButton('Join', url='https://t.me/join')
+        # Send log in UPDATE_CHANNEL with IMDB_TEMPLATE and IMDB poster
+        if imdb:
+            buttons = [
+                [
+                    InlineKeyboardButton('Join', url='https://t.me/join')
+                ]
             ]
-        ]
-        TEMPLATE = IMDB_TEMPLATE
-        cap = TEMPLATE.format(
-            query=search_query,
-            title=imdb['title'],
-            votes=imdb['votes'],
-            aka=imdb["aka"],
-            seasons=imdb["seasons"],
-            box_office=imdb['box_office'],
-            localized_title=imdb['localized_title'],
-            kind=imdb['kind'],
-            imdb_id=imdb["imdb_id"],
-            cast=imdb["cast"],
-            runtime=imdb["runtime"],
-            countries=imdb["countries"],
-            certificates=imdb["certificates"],
-            languages=imdb["languages"],
-            director=imdb["director"],
-            writer=imdb["writer"],
-            producer=imdb["producer"],
-            composer=imdb["composer"],
-            cinematographer=imdb["cinematographer"],
-            music_team=imdb["music_team"],
-            distributors=imdb["distributors"],
-            release_date=imdb['release_date'],
-            year=imdb['year'],
-            genres=imdb['genres'],
-            poster=imdb['poster'],
-            plot=imdb['plot'],
-            rating=imdb['rating'],
-            url=imdb['url'],
-            **locals()
-        )
+            TEMPLATE = IMDB_TEMPLATE
+            cap = TEMPLATE.format(
+                query=search_query,
+                title=imdb['title'],
+                votes=imdb['votes'],
+                aka=imdb["aka"],
+                seasons=imdb["seasons"],
+                box_office=imdb['box_office'],
+                localized_title=imdb['localized_title'],
+                kind=imdb['kind'],
+                imdb_id=imdb["imdb_id"],
+                cast=imdb["cast"],
+                runtime=imdb["runtime"],
+                countries=imdb["countries"],
+                certificates=imdb["certificates"],
+                languages=imdb["languages"],
+                director=imdb["director"],
+                writer=imdb["writer"],
+                producer=imdb["producer"],
+                composer=imdb["composer"],
+                cinematographer=imdb["cinematographer"],
+                music_team=imdb["music_team"],
+                distributors=imdb["distributors"],
+                release_date=imdb['release_date'],
+                year=imdb['year'],
+                genres=imdb['genres'],
+                poster=imdb['poster'],
+                plot=imdb['plot'],
+                rating=imdb['rating'],
+                url=imdb['url'],
+                **locals()
+            )
 
-        if imdb.get('poster'):
-            try:
-                await bot.send_photo(chat_id=UPDATE_CHANNEL, photo=imdb['poster'], caption=cap, reply_markup=InlineKeyboardMarkup(buttons))
-            except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-                poster = imdb['poster'].replace('.jpg', '._V1_UX360.jpg')
-                await bot.send_photo(chat_id=UPDATE_CHANNEL, photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(buttons))
-            except Exception as e:
-                logger.exception(e)
+            if imdb.get('poster'):
+                try:
+                    await bot.send_photo(chat_id=UPDATE_CHANNEL, photo=imdb['poster'], caption=cap, reply_markup=InlineKeyboardMarkup(buttons))
+                except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+                    poster = imdb['poster'].replace('.jpg', '._V1_UX360.jpg')
+                    await bot.send_photo(chat_id=UPDATE_CHANNEL, photo=poster, caption=cap, reply_markup=InlineKeyboardMarkup(buttons))
+                except Exception as e:
+                    logger.exception(e)
+                    await bot.send_message(chat_id=UPDATE_CHANNEL, text=cap, reply_markup=InlineKeyboardMarkup(buttons))
+            else:
                 await bot.send_message(chat_id=UPDATE_CHANNEL, text=cap, reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await bot.send_message(chat_id=UPDATE_CHANNEL, text=cap, reply_markup=InlineKeyboardMarkup(buttons))
+            await bot.send_message(chat_id=UPDATE_CHANNEL, text=f"🆕️ New File Added in Database\n{search_query}")
     else:
-        await bot.send_message(chat_id=UPDATE_CHANNEL, text=f"Here is what I found for your query {search_query}")
+        await bot.send_message(chat_id=UPDATE_CHANNEL, text=f"🆕️ New File Added in Database\n{search_query}")
