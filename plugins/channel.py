5fa27bcac67e2ebcbb183eeb4f7047b865906108
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-07 17:48:31 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -22,59 +22,44 @@ async def media(bot, message):
     await save_file(media)
 
     # Extracting the search query from the file name
-    full_file_name = re.sub(r'[^\w\d]+', ' ', media.file_name)  # Update: Remove special characters from full_file_name
+    full_file_name = media.file_name.replace('_', ' ').replace('.', ' ')
     file_name = ""
 
-    # Extracting the part before year or series season if any
-    file_name_match = re.search(r'^.*?(?=\d{4}\b|\bS\d+\b)', full_file_name)
-    if file_name_match:
-        file_name = file_name_match.group().strip()
-    else:
-        # If year or series season not found, check for language in curly brackets
-        language_match = re.search(r'\{(.*?)\}', full_file_name)
-        if language_match:
-            file_name = full_file_name.replace(language_match.group(), '').strip()
-        else:
-            # If language not found in curly brackets, check for categories or words before a number
-            file_name_match = re.search(r'^.*?(?=\b\d+\b)', full_file_name)
-            if file_name_match:
-                file_name = file_name_match.group().strip()
-            else:
-                # If no year, season, or language found, use the entire file name as file_name
-                file_name = full_file_name
+    # Detecting the year in 4-digit number format
+    year_match = re.search(r'\b\d{4}\b', full_file_name)
+    series_season_match = re.search(r'\bS\d+\b', full_file_name)
+
+    if year_match or series_season_match:
+        # Extracting the part before year or series season
+        file_name = re.search(r'^.*?(?=\d{4}\b|\bS\d+\b)', full_file_name).group().strip()
+
+    if not file_name:
+        # If no year or series season is found, use the entire file name
+        file_name = full_file_name
 
     # Detecting video resolution
     video_resolution_match = re.search(r'\b\d{3,4}p\b', file_name)
     video_resolution = video_resolution_match.group() if video_resolution_match else None
 
-    # Detecting languages in the file name using regular expressions
-    languages_match = re.search(r'{(.*?)}', file_name)
-    languages = languages_match.group(1) if languages_match else None
+    if year_match:
+        # Remove the year from the file name
+        file_name_without_year = file_name.replace(year_match.group(), '').strip()
 
-    if not languages:
-        # If no languages found in curly brackets, try to detect languages without curly brackets
-        languages_match = re.search(r'\b(?:English|Hindi|Bengali|Telugu|Marathi|Tamil|Urdu|Gujarati|Kannada|Odia|Punjabi|Malayalam|Spanish|French|German|Italian|Russian|Korean|Chinese)\b', file_name, re.IGNORECASE)
-        languages = languages_match.group() if languages_match else None
-
-    if languages:
-        # Remove the language portion from the file name
-        file_name_without_languages = file_name.replace(languages_match.group(), '').strip()
-
-        # Combine the file name without languages and year for IMDB search
-        search_query = f"{file_name_without_languages} {file_name_match.group()}" if file_name_match else file_name_without_languages
+        # Combine the file name and year for IMDB search
+        search_query = f"{file_name_without_year} {year_match.group()}"
     else:
-        # If no languages found, use the entire file name for the search query
+        # If the year is not found in the file name, use the entire file name for the search query
         search_query = file_name
 
-    # Get the IMDB data and poster based on the search query
+     # Get the IMDB data and poster based on the search query
     imdb = await get_poster(search_query)
 
-    # Check for exact match and send IMDB data and poster to UPDATE_CHANNEL
-    if imdb and imdb['title'] == file_name and imdb['year'] == file_name_match.group():
+    # Send log in UPDATE_CHANNEL with IMDB_TEMPLATE and IMDB poster
+    if imdb:
         buttons = [
             [
-                InlineKeyboardButton('Join Channel', url='https://t.me/PremiumMHBot'),
-                InlineKeyboardButton('Join Group', url='https://t.me/PremiumMHBot')
+                InlineKeyboardButton('Join', url='https://t.me/PremiumMHBot'),
+                InlineKeyboardButton('Join', url='https://t.me/PremiumMHBot')
             ],
         ]
         TEMPLATE = IMDB_TEMPLATE
@@ -122,10 +107,7 @@ async def media(bot, message):
         else:
             await bot.send_message(chat_id=UPDATE_CHANNEL, text=cap, reply_markup=InlineKeyboardMarkup(buttons))
     else:
-        # If IMDB data and poster not found, send the specific message format
-        language = languages if languages else "Unknown"
-        year = file_name_match.group() if file_name_match else "Unknown"
-        video_resolution = video_resolution if video_resolution else "Unknown"
-        text = f"New File Added In Bot\n\n🏷 Title: {file_name}\n🎭 Genres: {language}\n📆 Year: {year}\n🌟 Video resolution: {video_resolution}"
-        await bot.send_message(chat_id=UPDATE_CHANNEL, text=text)
-
+        await bot.send_message(chat_id=UPDATE_CHANNEL, text=f"New File Added In Bot\n{file_name}")
+        
+        
+        
