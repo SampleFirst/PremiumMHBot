0640291ec7ec43ad618c83448b042ad551a8f522
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-08-18 19:16:07 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,14 +1,14 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from pyrogram.types import InlineKeyboardButton
-from pyrogram import Client, filters
+from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
+from pyrogram.types import Message, InlineKeyboardButton
+from pyrogram import Client, filters, enums
 import datetime
 import time, os
 from database.users_chats_db import db
 from info import ADMINS
-
-logging.basicConfig(level=logging.INFO)
-
+import asyncio
+        
 @Client.on_message(filters.command("mention_group_broadcast") & filters.user(ADMINS) & filters.reply)
 async def mention_group_broadcast(bot, message):
     groups = await db.get_all_chats()
@@ -20,19 +20,19 @@ async def mention_group_broadcast(bot, message):
     failed = ""
     success = 0
     deleted = 0
-    for group in groups:
-        pti, sh, ex = await broadcast_messages_group_with_mentions(bot, int(group['id']), b_msg)
-        if pti:
+    async for group in groups:
+        pti, sh, ex = await broadcast_messages_group_with_mentions(int(group['id']), b_msg)
+        if pti == True:
             if sh == "Success":
                 success += 1
-        else:
+        elif pti == False:
             if sh == "deleted":
                 deleted += 1
                 failed += ex
                 try:
                     await bot.leave_chat(int(group['id']))
                 except Exception as e:
-                    logging.error(f"{e} > {group['id']}")
+                    print(f"{e} > {group['id']}")
         done += 1
         if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}\nDeleted: {deleted}")
@@ -46,7 +46,7 @@ async def mention_group_broadcast(bot, message):
         await message.reply_document('reason.txt', caption=f"Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}\nDeleted: {deleted}")
         os.remove("reason.txt")
 
-async def broadcast_messages_group_with_mentions(bot, group_id, message):
+async def broadcast_messages_group_with_mentions(group_id, message):
     try:
         group = await bot.get_chat(group_id)
         mention_text = create_mention_text(group)
@@ -61,4 +61,3 @@ def create_mention_text(group):
         if member.user.username:
             mention_text += f"@{member.user.username} "
     return mention_text
-    
