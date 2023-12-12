f0860102c69d12ea3f9fb389ec6e2a15d0becf7c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-12-12 19:59:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -15,7 +15,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 
 from info import (
-    ADMINS, PICS
+    ADMINS, PICS, MSG_ALRT
 )
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
