bdd0863fb1db944eba188604f511c401bf47e4bb
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-12-29 18:32:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -21,8 +21,8 @@ logger.setLevel(logging.ERROR)
 # Define a dictionary to store user states (locked or not)
 user_states = {}
 USER_SELECTED = {}
-TOTAL_SEAT_BOT = 500
-DAILY_SEAT_BOT = 1
+TOTAL_SEAT_BOT = 1
+DAILY_SEAT_BOT = 2
 
 @Client.on_message(filters.photo & filters.private)
 async def payment_screenshot_received(client, message):
