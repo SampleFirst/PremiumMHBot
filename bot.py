27fbf630ef67cbae8d18bbbc2c391cfd5551d3aa
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-01-29 18:09:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,6 +54,11 @@ class Bot(Client):
         date = curr.strftime('%d %B, %Y')
         time = curr.strftime('%I:%M:%S %p')
         await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(date, time))
+        await self.send_message(chat_id=ADMINS, text="hii Admin Bot Restarted")
+
+        await asyncio.sleep(30)
+        await self.send_message(chat_id=ADMINS, text="Hey Admin! Bot is working normally. Send /start.")
+
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
