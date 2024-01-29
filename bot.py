1190ecc77e44dcb8fa8daf8a9120b8018b41a884
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-01-29 22:52:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,12 +64,9 @@ class Bot(Client):
             except Exception as e:
                 logger.warning(f"Bot Isn't Able To Send Message To Admin {admin_id} \n{e}")
 
-        await asyncio.sleep(30)
-            try:
-                await self.send_message(chat_id=ADMINS, text="Hey Admin! Bot is working normally. Send /start.")
-            except Exception as e:
-                logger.warning(f"Bot Isn't Able To Send Message To Admin {admin_id} \n{e}")
-
+        await asyncio.sleep(30)    
+        await self.send_message(chat_id=ADMINS, text="Hey Admin! Bot is working normally. Send /start.")
+        
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
