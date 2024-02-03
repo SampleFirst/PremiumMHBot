3a8c6a6ad156f62fb17660224c6f9dcb56eea591
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-03 18:40:08 +0530

    Update update.py

--- plugins/update.py
+++ plugins/update.py
@@ -10,13 +10,14 @@ async def user_update(client, message):
         await message.reply("You don't have permission to use this command.")
         return
 
-    args = message.get_args().split()
-    if len(args) != 1:
-        await message.reply("Usage: /latestupdate <user_id>")
+    args = message.text.split()
+    if len(args) != 2:
+        await message.reply("Usage: /userupdate <user_id>")
         return
 
-    user_id = int(args[0])
-    if user_update = await db.get_latest_user_update(user_id):
+    user_id = int(args[1])
+    user_update = await db.get_latest_user_update(user_id)
+    if user_update:
         await message.reply(f"Latest update for user {user_id}:\n{user_update}")
     else:
         await message.reply(f"No user found with ID {user_id}")
@@ -24,7 +25,8 @@ async def user_update(client, message):
 
 @Client.on_message(filters.command("myupdate"))
 async def my_update(client, message):
-    if user_update = await db.get_latest_user_update(message.from_user.id):
+    user_update = await db.get_latest_user_update(message.from_user.id)
+    if user_update:
         await message.reply(f"Your latest update:\n{user_update}")
     else:
         await message.reply("No update data found for your user ID.")
