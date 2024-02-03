88547d701fea461b943eba865dea37aa70fab5ed
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-03 23:13:28 +0530

    Update unrecognized.py

--- plugins/unrecognized.py
+++ plugins/unrecognized.py
@@ -1,11 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-import asyncio
-from info import LOG_CHANNEL
 
-@Client.on_message(filters.text & filters.command(""))
-async def unrecognized_command(client, message):
-    await client.send_message(message.chat.id, "Unrecognized command. What are you trying to say?")
-    await client.send_message(LOG_CHANNEL, f"{message.from_user.username} tried to execute the {message.text} command.")
-    await asyncio.sleep(2)
-    await message.delete()
+# Handle unrecognized commands
+@Client.on_message(filters.command)
+def unrecognized_command(client: Client, message: Message):
+    client.send_message(message.chat.id, "Unrecognized command. Type /help for a list of available commands.")
+
