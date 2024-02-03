e59f13401a93e8ab168c75a9410ee769936fe3af
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-03 18:34:02 +0530

    Update update.py

--- plugins/update.py
+++ plugins/update.py
@@ -4,8 +4,8 @@ from database.users_chats_db import db
 from info import ADMINS
 
 
-@Client.on_message(filters.command("latestupdate") & filters.user(ADMINS))
-async def handle_latest_update(client, message):
+@Client.on_message(filters.command("userupdate") & filters.user(ADMINS))
+async def user_update(client, message):
     if message.from_user.id not in ADMINS:
         await message.reply("You don't have permission to use this command.")
         return
@@ -16,15 +16,15 @@ async def handle_latest_update(client, message):
         return
 
     user_id = int(args[0])
-    if user_update := await db.get_latest_user_update(user_id):
+    if user_update = await db.get_latest_user_update(user_id):
         await message.reply(f"Latest update for user {user_id}:\n{user_update}")
     else:
         await message.reply(f"No user found with ID {user_id}")
 
 
 @Client.on_message(filters.command("myupdate"))
-async def handle_my_update(client, message):
-    if user_update := await db.get_latest_user_update(message.from_user.id):
+async def my_update(client, message):
+    if user_update = await db.get_latest_user_update(message.from_user.id):
         await message.reply(f"Your latest update:\n{user_update}")
     else:
         await message.reply("No update data found for your user ID.")
