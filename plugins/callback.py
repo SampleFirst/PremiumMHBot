494fd1c797a3893d43787f665a4787179eba2e91
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-07 00:21:54 +0530

    Update and rename pm_filter.py to callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,111 @@
+import random
+import logging
+
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+
+from info import ADMINS, PICS, LOG_CHANNEL
+from database.users_chats_db import db
+
+from Script import script
+from utils import temp
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.data == "cancel":
+        await query.message.delete()
+
+    elif query.data == "start":
+        buttons = [
+            [
+                InlineKeyboardButton('My Plan', callback_data="plan"),
+                InlineKeyboardButton('Status', callback_data="status")
+            ],
+            [
+                InlineKeyboardButton('Bots Premium', callback_data="bots"),
+                InlineKeyboardButton('Database Premium', callback_data="database")
+            ],
+            [
+                InlineKeyboardButton('Bots Pack', callback_data="botspack"),
+                InlineKeyboardButton('Database Pack', callback_data="dbpack")
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await client.edit_message_media(
+            query.message.chat.id,
+            query.message.id,
+            InputMediaPhoto(random.choice(PICS))
+        )
+        await query.message.edit_text(
+            text=script.START_TXT.format(user=query.from_user.mention, bot=temp.B_LINK),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    elif query.data == "plan":
+        await query.answer(text="Features under construction. Available soon...")
+
+    elif query.data == "status":
+        await query.answer(text="Processing...")
+
+    elif query.data == "bots":
+        buttons = [
+            [
+                InlineKeyboardButton('Movies Bot', callback_data='mbot'),
+                InlineKeyboardButton('Anime Bot', callback_data='abot')
+            ],
+            [
+                InlineKeyboardButton('Rename Bot', callback_data='rbot'),
+                InlineKeyboardButton('YT Downloader', callback_data='yibot')
+            ],
+            [
+                InlineKeyboardButton('Back', callback_data='start')
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await client.edit_message_media(
+            query.message.chat.id,
+            query.message.id,
+            InputMediaPhoto(random.choice(PICS))
+        )
+        await query.message.edit_text(
+            text=script.BOTS.format(user=query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    elif query.data == "database":
+        buttons = [
+            [
+                InlineKeyboardButton('Movies Database', callback_data='mdb'),
+                InlineKeyboardButton('Anime Database', callback_data='adb')
+            ],
+            [
+                InlineKeyboardButton('TV Show Database', callback_data='tvsdb'),
+                InlineKeyboardButton('Audio Books', callback_data='abdb')
+            ],
+            [
+                InlineKeyboardButton('Back', callback_data='start')
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await client.edit_message_media(
+            query.message.chat.id,
+            query.message.id,
+            InputMediaPhoto(random.choice(PICS))
+        )
+        await query.message.edit_text(
+            text=script.DATABSE.format(user=query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
+    elif query.data == "botspack":
+        await query.answer(text="Features under construction. Available soon...")
+
+    elif query.data == "dbpack":
+        await query.answer(text="Features under construction. Available soon...")
