35928be0186b485dcc442fb823bf5d8aafb1587d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-08 00:49:02 +0530

    Create get_name.py

---
+++ plugins/get_name.py
@@ -0,0 +1,30 @@
+# get_name.py
+import random
+import logging
+
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+
+async get_bot_name(query.data):
+    if query.data = "mbot"
+        bot_name = "Movies Bot"
+    elif query.data = "abot"
+        bot_name = "Anime Bot"
+    elif query.data = "rbot"
+        bot_name = "Rename Bot"
+    elif query.data = "dbot"
+        bot_name = "YouTube Downloader Bot"
+        
+async get_db_name(query.data):
+    if query.data = "mdb"
+        bot_name = "Movies Database"
+    elif query.data = "adb"
+        bot_name = "Anime Database"
+    elif query.data = "sdb"
+        bot_name = "TV Series Database"
+    elif query.data = "bdb"
+        bot_name = "Audio Book Database"
+    
