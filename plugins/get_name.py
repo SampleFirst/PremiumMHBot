ba3600c793059a572b59bad4731930160de69cd4
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-08 20:45:03 +0530

    Update get_name.py

--- plugins/get_name.py
+++ plugins/get_name.py
@@ -1,30 +1,32 @@
 # get_name.py
 import logging
 
-from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 
-async def get_bot_name(query.data):
-    if query.data = "mbot"
+async def get_bot_name(query):
+    bot_name = None
+    if query.data == "mbot":
         bot_name = "Movies Bot"
-    elif query.data = "abot"
+    elif query.data == "abot":
         bot_name = "Anime Bot"
-    elif query.data = "rbot"
+    elif query.data == "rbot":
         bot_name = "Rename Bot"
-    elif query.data = "dbot"
+    elif query.data == "dbot":
         bot_name = "YouTube Downloader Bot"
-        
-async def get_db_name(query.data):
-    if query.data = "mdb"
-        bot_name = "Movies Database"
-    elif query.data = "adb"
-        bot_name = "Anime Database"
-    elif query.data = "sdb"
-        bot_name = "TV Series Database"
-    elif query.data = "bdb"
-        bot_name = "Audio Book Database"
-        
+    return bot_name
+
+
+async def get_db_name(query):
+    db_name = None
+    if query.data == "mdb":
+        db_name = "Movies Database"
+    elif query.data == "adb":
+        db_name = "Anime Database"
+    elif query.data == "sdb":
+        db_name = "TV Series Database"
+    elif query.data == "bdb":
+        db_name = "Audio Book Database"
+    return db_name
+    
