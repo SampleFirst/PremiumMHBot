181deb2385a7fb904bb3f0b74da2e631cc75026d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-08 23:14:19 +0530

    Update get_name.py

--- plugins/get_name.py
+++ plugins/get_name.py
@@ -5,28 +5,28 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 
-async def get_bot_name(query.data):
+async def get_bot_name(query_data):
     bot_name = None
-    if query.data == "mbot":
+    if query_data == "mbot":
         bot_name = "Movies Bot"
-    elif query.data == "abot":
+    elif query_data == "abot":
         bot_name = "Anime Bot"
-    elif query.data == "rbot":
+    elif query_data == "rbot":
         bot_name = "Rename Bot"
-    elif query.data == "dbot":
+    elif query_data == "dbot":
         bot_name = "YouTube Downloader Bot"
     return bot_name
 
 
-async def get_db_name(query.data):
+async def get_db_name(query_data):
     db_name = None
-    if query.data == "mdb":
+    if query_data == "mdb":
         db_name = "Movies Database"
-    elif query.data == "adb":
+    elif query_data == "adb":
         db_name = "Anime Database"
-    elif query.data == "sdb":
+    elif query_data == "sdb":
         db_name = "TV Series Database"
-    elif query.data == "bdb":
+    elif query_data == "bdb":
         db_name = "Audio Book Database"
     return db_name
     
