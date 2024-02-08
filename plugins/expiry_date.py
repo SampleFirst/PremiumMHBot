ff9982cf89803063c40bf033a6e4302571c9cccb
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-08 14:42:50 +0530

    Update expiry_date.py

--- plugins/expiry_date.py
+++ plugins/expiry_date.py
@@ -18,6 +18,10 @@ def get_expiry_date(format_type, base_datetime=None, expiry_option=None, expiry_
             - "now_to_5m": Next 5 minutes
             - "now_to_10m": Next 10 minutes
             - "now_to_15m": Next 15 minutes
+            - "now_to_20m": Next 20 minutes
+            - "now_to_30m": Next 30 minutes
+            - "now_to_45m": Next 45 minutes
+            - "now_to_60m": Next 60 minutes
             - "today_to_1d": Tomorrow
             - "today_to_7d": Next week
             - "today_to_30d": Next month
@@ -42,55 +46,71 @@ def get_expiry_date(format_type, base_datetime=None, expiry_option=None, expiry_
     IST = pytz.timezone('Asia/Kolkata')
     now = datetime.now(IST)
 
-    format_type = get_datatime(format_type)
-    
-    # ... (existing code to format datetime based on format_type)
+    format_type = get_datetime(format_type)
+
+    expiry_options = {
+        "now_to_5": 5,
+        "now_to_10": 10,
+        "now_to_15": 15,
+        "now_to_20": 20,
+        "now_to_30": 30,
+        "now_to_45": 45,
+        "now_to_60": 60,
+        "today_to_1d": 1,
+        "today_to_7d": 7,
+        "today_to_30d": 30,
+        "today_to_60d": 60,
+        "today_to_90d": 90,
+        "today_to_180d": 180,
+        "today_to_365d": 365,
+    }
 
     # Calculate expiry date/time based on expiry_option
     if expiry_option:
-        if expiry_option.startswith("now_to_"):
-            delta_minutes = int(expiry_option.split("_")[2])
-            expiry_datetime = (now + timedelta(minutes=delta_minutes)).strftime("%Y-%m-%d %H:%M:%S")
-            if delta_minutes == 5:
-                expiry_name = "Next 5 minutes"
-            elif delta_minutes == 10:
-                expiry_name = "Next 10 minutes"
-            elif delta_minutes == 15:
-                expiry_name = "Next 15 minutes"
-            elif delta_minutes == 20:
-                expiry_name = "Next 20 minutes"
-            elif delta_minutes == 30:
-                expiry_name = "Next 30 minutes"
-            elif delta_minutes == 45:
-                expiry_name = "Next 45 minutes"
-            elif delta_minutes == 60:
-                expiry_name = "Next 60 minutes"
+        if expiry_option in expiry_options:
+            if expiry_option.startswith("now_to_"):
+                delta_minutes = expiry_options[expiry_option]
+                expiry_datetime = (now + timedelta(minutes=delta_minutes)).strftime("%Y-%m-%d %H:%M:%S")
+                if delta_minutes == 5:
+                    expiry_name = "Next 5 minutes"
+                elif delta_minutes == 10:
+                    expiry_name = "Next 10 minutes"
+                elif delta_minutes == 15:
+                    expiry_name = "Next 15 minutes"
+                elif delta_minutes == 20:
+                    expiry_name = "Next 20 minutes"
+                elif delta_minutes == 30:
+                    expiry_name = "Next 30 minutes"
+                elif delta_minutes == 45:
+                    expiry_name = "Next 45 minutes"
+                elif delta_minutes == 60:
+                    expiry_name = "Next 60 minutes"
+                else:
+                    expiry_name = f"In {minutes} Minutes"
+            elif expiry_option.startswith("today_to_"):
+                delta_days = expiry_options[expiry_option]
+                expiry_datetime = (now + timedelta(days=delta_days)).strftime("%Y-%m-%d %H:%M:%S")
+                if delta_days == 1:
+                    expiry_name = "Tomorrow"
+                elif delta_days == 7:
+                    expiry_name = "Next week"
+                elif delta_days == 30:
+                    expiry_name = "Next month"
+                elif delta_days == 60:
+                    expiry_name = "Next 2 months"
+                elif delta_days == 90:
+                    expiry_name = "Next quarter"
+                elif delta_days == 180:
+                    expiry_name = "Next 6 months"
+                elif delta_days == 365:
+                    expiry_name = "Next year"
+                else:
+                    expiry_name = f"In {days} days"
             else:
-                expiry_name = f"In {minutes} Minutes"
-        elif expiry_option.startswith("today_to_"):
-            delta_days = int(expiry_option.split("_")[2])
-            expiry_datetime = (now + timedelta(days=delta_days)).strftime("%Y-%m-%d %H:%M:%S")
-            if delta_days == 1:
-                expiry_name = "Tomorrow"
-            elif delta_days == 7:
-                expiry_name = "Next week"
-            elif delta_days == 30:
-                expiry_name = "Next month"
-            elif delta_days == 60:
-                expiry_name = "Next 2 months"
-            elif delta_days == 90:
-                expiry_name = "Next quarter"
-            elif delta_days == 180:
-                expiry_name = "Next 6 months"
-            elif delta_days == 365:
-                expiry_name = "Next year"
-            else:
-                expiry_name = f"In {days} days"
+                raise ValueError(f"Invalid expiry_option: {expiry_option}")
         else:
-            raise ValueError(f"Invalid expiry_option: {expiry_option}")
-    else:
-        expiry_datetime = None
-        expiry_name = None
+            expiry_datetime = None
+            expiry_name = None
 
     return formatted_datetime, expiry_datetime, expiry_name
 
