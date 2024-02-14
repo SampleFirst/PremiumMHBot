907d117b1db78195628f7f23fbc30875c63ec6ae
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-14 21:50:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,17 +7,16 @@ logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
-import asyncio
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import ADMINS, SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-from Script import script
-from datetime import date, datetime
+from Script import script 
+from datetime import date, datetime 
 import pytz
 from aiohttp import web
 from plugins import web_server
@@ -46,25 +45,15 @@ class Bot(Client):
         temp.ME = me.id
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
-        temp.B_LINK = me.mention
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
         logging.info(script.LOGO)
         tz = pytz.timezone('Asia/Kolkata')
-        curr = datetime.now(tz)
-        date = curr.strftime('%d %B, %Y')
-        time = curr.strftime('%I:%M:%S %p')
-        try:
-            await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(date, time))        
-        except Exception as e:
-            logger.warning(f"Bot Isn't Able To Send Message To LOG_CHANNEL \n{e}")
-
-        for admin_id in ADMINS:
-            await self.send_message(admin_id, text=f"Bot started successfully at {date}, {time}.")
-            await asyncio.sleep(30)  # Wait for 30 seconds
-            await self.send_message(admin_id, text="Bot Now Working.\nsend command /start")
-            
+        today = date.today()
+        now = datetime.now(tz)
+        time = now.strftime("%H:%M:%S %p")
+        await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
