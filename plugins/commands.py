96219e9f793506309efefaaeaae6f34ba52574ed
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-17 16:09:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,6 +6,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.users_chats_db import db
+from database.attempt_db import db
 from info import ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS
 from utils import is_subscribed, temp
 from datetime import date, datetime
@@ -103,3 +104,15 @@ async def log_file(bot, message):
         await message.reply_document('Logs.txt')
     except Exception as e:
         await message.reply(str(e))
+
+@Client.on_message(filters.command('remove_attempts'))
+async def update_attempts(bot, message):
+    """Updates active attempts for the user who sent the command."""
+    user_id = message.from_user.id
+
+    try:
+        await db.update_active_attempts(user_id)
+        await message.reply_text("All active attempts have been updated.")
+    except Exception as e:
+        logger.error(f"Error updating attempts: {e}")
+        await message.reply_text(f"An error occurred: {e}")
