56deef030c70c3ed5074e536e3d3c34e628a37ee
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-17 16:37:06 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -13,7 +13,7 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
-
+        self.att = self.db.attempt
 
     def new_user(self, id, name):
         tz = pytz.timezone('Asia/Kolkata')
@@ -40,6 +40,24 @@ class Database:
             timestamp=datetime.now(tz)
         )
     
+    def new_attempt(self, id, name, is_att, att_active, att_name, att_type, att_date, att_validity):
+        tz = pytz.timezone('Asia/Kolkata')  
+        return dict(
+            id=id,
+            name=name,
+            is_att=is_att,
+            att_active=att_active,
+            att_name=att_name,
+            att_type=att_type,
+            att_date=att_date,
+            att_validity=att_validity,
+            cancel_status=dict(
+                is_cancel=False,
+                ban_reason="",
+            ),
+            timestamp=datetime.now(tz)
+        )
+        
     async def update_verification(self, id, date, time):
         status = {
             'date': str(date),
@@ -200,4 +218,64 @@ class Database:
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
 
+    async def add_attempt(self, id, name, att_name, att_type, att_date, att_validity):
+        attempt = self.new_attempt(
+            id=id, 
+            name=name,
+            is_att=True,
+            att_active=True,
+            att_name=att_name, 
+            att_type=att_type, 
+            att_date=att_date,
+            att_validity=att_validity,
+        )
+        await self.att.insert_one(attempt)
+
+    async def is_attempt_active(self, id, att_name, att_type):
+        last_attempt = await self.att.find_one({'att_name': att_name, 'att_type': att_type}, sort=[('_id', -1)])
+        if last_attempt:
+            return last_attempt['att_active']
+        return None
+
+    async def total_attempts_count(self, att_name=None, att_type=None):
+        query = {}
+        if att_name:
+            query['att_name'] = att_name
+        if att_type:
+            query['att_type'] = att_type
+        count = await self.att.count_documents(query)
+        return count
+        
+    async def expire_attempts(self):
+        tz = pytz.timezone('Asia/Kolkata')
+        now = datetime.now(tz)
+        expired_attempts = await self.att.find({'att_expiry': {'$lte': now}, 'att_active': True}).to_list(None)
+        
+        for attempt in expired_attempts:
+            await self.att.update_one({'_id': attempt['_id']}, {'$set': {'att_active': False}})
+            
+    async def get_all_attempts(self):
+        return self.att.find({})
+        
+        
+    async def daily_attempts_count(self, today, att_name=None):
+        tz = pytz.timezone('Asia/Kolkata')
+        start = tz.localize(datetime.combine(today, datetime.min.time()))
+        end = tz.localize(datetime.combine(today, datetime.max.time()))
+        query = {'timestamp': {'$gte': start, '$lt': end}}
+        if att_name:
+            query['att_name'] = att_name
+        count = await self.att.count_documents(query)
+        return count
+    
+    async def monthly_attempts_count(self, month, year, att_name=None):
+        tz = pytz.timezone('Asia/Kolkata')
+        start = tz.localize(datetime.combine(datetime(year, month, 1), datetime.min.time()))
+        end = tz.localize(datetime.combine(datetime(year, month, calendar.monthrange(year, month)[1]), datetime.max.time()))
+        query = {'timestamp': {'$gte': start, '$lt': end}}
+        if att_name:
+            query['att_name'] = att_name
+        count = await self.att.count_documents(query)
+        return count
+        
 db = Database(DATABASE_URI, DATABASE_NAME)
