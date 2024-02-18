9f0fef2e5f64f26b628935463920d139e8a558b4
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-18 20:10:45 +0530

    Update mkv_cinemas.py

--- plugins/mkv_cinemas.py
+++ plugins/mkv_cinemas.py
@@ -3,14 +3,15 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
 from bs4 import BeautifulSoup
 from io import BytesIO
+from info import ADMINS 
+
 
 url_list = {}
 api_key = "3269bf2096dd8aec64d201398176d3db93ce68db"
 
 
-
 @Client.on_message(filters.command("mkvcinemas"))
-def find_movie(client, message):
+def mkvcinemas(client, message):
     query = message.text.split(maxsplit=1)
     if len(query) == 1:
         message.reply_text("Please provide a movie name to search.")
@@ -31,39 +32,40 @@ def find_movie(client, message):
 @Client.on_callback_query()
 def movie_result(client, callback_query):
     query = callback_query
-    s = get_movie(query.data)
+    s = get_movie(query.data, is_admin(callback_query.from_user.id))
     response = requests.get(s["img"])
     img = BytesIO(response.content)
-    query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
+    callback_query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
     link = ""
     links = s["links"]
     for i in links:
-        link += "🎬" + i + "\n" + links[i] + "\n\n"
+        link += f"🎬 {i}\n{links[i]}\n\n"
     caption = f"⚡ Fast Download Links :-\n\n{link}"
     if len(caption) > 4095:
         for x in range(0, len(caption), 4095):
-            query.message.reply_text(text=caption[x:x+4095])
+            callback_query.message.reply_text(text=caption[x:x + 4095])
     else:
-        query.message.reply_text(text=caption)
+        callback_query.message.reply_text(text=caption)
+
 
 def search_movies(query):
     movies_list = []
-    movies_details = {}
     website = requests.get(f"https://mkvcinemas.dev/?s={query.replace(' ', '+')}")
     if website.status_code == 200:
         website = website.text
         website = BeautifulSoup(website, "html.parser")
         movies = website.find_all("a", {'class': 'ml-mask jt'})
-        for movie in movies:
+        for index, movie in enumerate(movies):
+            movies_details = {}
             if movie:
-                movies_details["id"] = f"link{movies.index(movie)}"
+                movies_details["id"] = f"link{index}"
                 movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
                 url_list[movies_details["id"]] = movie['href']
-            movies_list.append(movies_details)
-            movies_details = {}
+                movies_list.append(movies_details)
     return movies_list
 
-def get_movie(query):
+
+def get_movie(query, is_admin):
     movie_details = {}
     movie_page_link = requests.get(f"{url_list[query]}")
     if movie_page_link.status_code == 200:
@@ -76,11 +78,12 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
-            response = requests.get(url)
-            link = response.json()
-            final_links[f"{i.text}"] = link['shortenedUrl']
+            if is_admin:
+                final_links[i.text] = i['href']
+            else:
+                url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
+                response = requests.get(url)
+                link = response.json()
+                final_links[i.text] = link['shortenedUrl']
         movie_details["links"] = final_links
     return movie_details
-
-
