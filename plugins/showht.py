858d2b088e080c50c4b8ff15dcadd3831cb10029
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-19 22:19:06 +0530

    Update showht.py

--- plugins/showht.py
+++ plugins/showht.py
@@ -3,7 +3,7 @@ import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import logging
-from info import ADMINS, LOG_CHANNEL 
+from info import ADMINS, LOG_CHANNEL
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -12,11 +12,16 @@ def get_domain(webpage):
     return webpage.split("//")[1].split("/")[0]
 
 # Function to handle messages containing URLs
-@Client.on_message(filters.command("gethtml"))
+@Client.on_message(filters.command("gethtml") & filters.private)
 async def get_html(client, message):
-    webpage = message.text.split()[1]
-
     try:
+        # Check if the command has the URL parameter
+        if len(message.command) != 2:
+            await message.reply_text("Please provide a URL.")
+            return
+
+        webpage = message.text.split()[1]
+
         msg = await message.reply_text(f"Fetching HTML code for {webpage}...")
 
         response = requests.get(webpage)
@@ -31,7 +36,7 @@ async def get_html(client, message):
         # Create the text file and send it to the user
         with open(file_path, "w") as f:
             f.write(html_code)
-        
+
         # Delete the temporary message
         await msg.delete()
         
@@ -40,7 +45,7 @@ async def get_html(client, message):
             caption=caption
         )
 
-        await message.send_document(
+        await client.send_document(
             chat_id=LOG_CHANNEL,
             document=file_path,
             caption=caption
@@ -48,8 +53,7 @@ async def get_html(client, message):
 
         # Delete the temporary file
         os.remove(file_path)
-        
+
     except Exception as e:
-        print(e)
         await message.reply_text(f"Error fetching HTML code: {e}")
 
