95fc347fafc9a46c8144694912d4343c2a015bee
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-19 15:34:10 +0530

    Update showht.py

--- plugins/showht.py
+++ plugins/showht.py
@@ -2,27 +2,30 @@ import os
 import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import logging
+
+logging.basicConfig(level=logging.ERROR)
 
 # Function to extract the domain name from a URL
 def get_domain(webpage):
     return webpage.split("//")[1].split("/")[0]
 
 
-
 # Function to handle inline keyboard button presses
 @Client.on_callback_query()
 async def handle_callback_webpage_query(client, callback_query):
-    webpage = callback_query.data
-    await callback_query.message.edit_text(
-        f"Fetching HTML code for {webpage}...", reply_markup=InlineKeyboardMarkup()
-    )
-
     try:
+        webpage = callback_query.data
+        await callback_query.message.edit_text(
+            f"Fetching HTML code for {webpage}...", reply_markup=InlineKeyboardMarkup()
+        )
+    
         response = requests.get(webpage)
         response.raise_for_status()  # Raise an exception if the request fails
         html_code = response.text
 
         # Replace domain_name with the actual domain extracted from the URL
+        domain_name = get_domain(webpage)
         file_path = f"html_code_{domain_name}.txt"
         with open(file_path, "w") as f:
             f.write(html_code)
@@ -31,7 +34,8 @@ async def handle_callback_webpage_query(client, callback_query):
             f"HTML code fetched successfully! Saved to: {file_path}\nFeel free to send another website link or press /start to restart."
         )
 
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
+        print(e)  # print the error message
         await callback_query.message.edit_text(f"Error fetching HTML code: {e}")
 
 # Function to handle messages containing URLs
@@ -40,7 +44,7 @@ async def handle_url_message(client, message):
     webpage = message.text
 
     # Check if the domain is allowed (add any restrictions as needed)
-    if get_domain(webpage) not in ("https://skymovieshd.ngo"):  # Adjust this list as required
+    if get_domain(webpage) not in ("skymovieshd.ngo"):  # Adjust this list as required
         await message.reply_text(
             "This website is not currently allowed. Please send a link from a supported website."
         )
