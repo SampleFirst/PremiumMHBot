6859cf104b07b8444cfd59d0abe3dd657689ebe0
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-20 22:32:16 +0530

    Update letest.py

--- plugins/letest.py
+++ plugins/letest.py
@@ -3,22 +3,22 @@ from bs4 import BeautifulSoup
 from pyrogram import Client, filters
 
 
-@Client.on_message(filters.command("latest"))
-async def latest_movies(client, message):
-    await message.reply_text("Fetching latest movies...")
+@Client.on_message(filters.command("popular"))
+async def popular_movies(client, message):
+    msg = await message.reply_text("Fetching popular movies...")
     url = "https://skymovieshd.ngo/"
 
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise an exception for HTTP errors
         soup = BeautifulSoup(response.text, "html.parser")
-        movies = soup.find_all('div', class_='Fmvideo')
+        movies = soup.find_all('div', class_='Let')
         movie_list = ""
         for movie in movies:
             movie_list += f"{movie.text}\n\n"
 
-        await message.reply_text(f"Latest Updated Movies:\n{movie_list}")
+        await msg.delete()
+        await message.reply_text(f"Most Popular Movies:\n{movie_list}")
 
     except Exception as e:
         await message.reply_text(f"An error occurred: {e}")
-
