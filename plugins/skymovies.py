8d5bb2a6952954fa007cac3dc4b5c6ea63eb20a6
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-23 00:34:29 +0530

    Update and rename mkvcinema.py to skymovies.py

--- plugins/skymovies.py
+++ plugins/skymovies.py
@@ -1,11 +1,8 @@
-import logging
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
 from bs4 import BeautifulSoup
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 movie_links = {}
 ddl_links = {}
@@ -15,7 +12,7 @@ ddl_links = {}
 async def skymovies(client, message):
     query = message.text.split(maxsplit=1)
     if len(query) == 1:
-        await message.reply_text("Please provide a movie name to search.")
+        await message.reply_text("Please provide a movie name to search.", quote=True)
         return
     query = query[1]
     search_results = await message.reply_text("Processing...")
@@ -26,16 +23,16 @@ async def skymovies(client, message):
             keyboard = [InlineKeyboardButton(movie["title"], callback_data=movie["id"])]
             keyboards.append(keyboard)
         reply_markup = InlineKeyboardMarkup(keyboards)
-        await search_results.edit_text('Search Results...', reply_markup=reply_markup)
+        await search_results.edit_text('Search Results...', reply_markup=reply_markup, quote=True)
     else:
-        await search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
+        await search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.', quote=True)
 
 
 @Client.on_callback_query(filters.regex('^len'))
 async def movie_result(client, callback_query):
     query = callback_query
     movie_id = query.data
-    msg = await query.message.reply_text("Processing...")
+    msg = await query.message.reply_text("Searching download link group...")
     download_list = get_movie(movie_links[movie_id])
     if download_list:
         keyboards = []
@@ -45,6 +42,7 @@ async def movie_result(client, callback_query):
         reply_markup = InlineKeyboardMarkup(keyboards)
         await msg.delete()
         await query.message.reply_text("Choose Download Link:", reply_markup=reply_markup)
+        await query.answer("Send download links..")
     else:
         await query.answer("No download links available for this movie.")
 
@@ -53,21 +51,20 @@ async def movie_result(client, callback_query):
 async def open_link_and_extract(client, callback_query):
     query = callback_query
     download_id = query.data
-    msg = await query.message.reply_text("Processing...")
+    msg = await query.message.reply_text("Searching finale Download links...")
     finale_list = final_link_page(ddl_links[download_id])
-    logger.info(f"Searching for: {finale_list}")
     keyboards = []
     for link in finale_list:
-        domain = link.split("//")[-1].split("/")[0]
-        keyboard = [InlineKeyboardButton(domain, url=link)]
-        keyboards.append([keyboard])
+        keyboard = [InlineKeyboardButton(link['text'], url=link['link'])]
+        keyboards.append(keyboard)
     reply_markup = InlineKeyboardMarkup(keyboards)
     await msg.delete()
     await query.message.reply_text("Extracted Links:", reply_markup=reply_markup)
+    await query.answer("Send Final Download links..")
+
     
 
 def search_movies(query):
-    logger.info(f"Searching for movies with query: {query}")
     movies_list = []
     website = requests.get(f"https://skymovieshd.ngo/search.php?search={query.replace(' ', '+')}&cat=All")
     if website.status_code == 200:
@@ -89,7 +86,6 @@ def get_movie(movie_page_url):
     download_list = []
     movie_page = "https://skymovieshd.ngo" + movie_page_url
     movie_page_link = requests.get(movie_page)
-    logger.info(f"Searching first download link groups: {movie_page}")
     if movie_page_link.status_code == 200:
         movie_page_link = movie_page_link.text
         movie_page_link = BeautifulSoup(movie_page_link, "html.parser")
@@ -109,7 +105,6 @@ def final_link_page(download_page_url):
     finale_list = []
     download_page = download_page_url
     webpage = requests.get(download_page)
-    logger.info(f"Searching final download link: {download_page}")
     if webpage.status_code == 200:
         webpage = webpage.text
         webpage = BeautifulSoup(webpage, "html.parser")
@@ -117,7 +112,7 @@ def final_link_page(download_page_url):
         for link in links:
             href = link['href']
             if href.startswith("https://"):
-                finale_list.append(href)
+                domain = href.split("//")[-1].split("/")[0]
+                finale_list.append({'link': href, 'text': domain})
     return finale_list
-    
-    
+
