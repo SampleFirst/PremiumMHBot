326ab699aff2a211dcb1eb9516cee948a067526c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-24 20:28:43 +0530

    Update gethtml.py

--- plugins/gethtml.py
+++ plugins/gethtml.py
@@ -7,53 +7,43 @@ from info import ADMINS, LOG_CHANNEL
 
 logging.basicConfig(level=logging.ERROR)
 
-# Function to extract the domain name from a URL
-def get_domain(webpage):
-    return webpage.split("//")[1].split("/")[0]
 
-# Function to handle messages containing URLs
+def get_domain(url):
+    return url.split("//")[1].split("/")[0]
+
+
+
 @Client.on_message(filters.command("gethtml") & filters.private)
 async def get_html(client, message):
     try:
-        # Check if the command has the URL parameter
         if len(message.command) != 2:
             await message.reply_text("Please provide a URL.")
-            return
-
-        webpage = message.text.split()[1]
-
-        msg = await message.reply_text(f"Fetching HTML code for {webpage}...")
-
-        response = requests.get(webpage)
-        response.raise_for_status()  # Raise an exception if the request fails
-        html_code = response.text
-
-        domain_name = get_domain(webpage)
-        file_path = f"{domain_name}.html"
-
-        caption = f"HTML code sent successfully!\n\nDomain Name: {domain_name}\n\nWebsite: {webpage}"
-
-        # Create the text file and send it to the user
+            return        
+        usr_agent = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/61.0.3163.100 Safari/537.36'
+        }
+        url = message.text.split()[1]
+        msg = await message.reply_text(f"Fetching HTML code for {url}...")
+        response = requests.get(url, headers=usr_agent)
+        response.raise_for_status()
+        html = response.text
+        domain = get_domain(url)
+        file_path = f"{domain}.html"
+        caption = f"HTML code sent successfully!\n\nDomain Name: {domain}\n\nWebsite: {url}"
         with open(file_path, "w") as f:
-            f.write(html_code)
-
-        # Delete the temporary message
-        await msg.delete()
-        
+            f.write(html)
+        await msg.delete()        
         await message.reply_document(
             document=file_path,
             caption=caption
         )
-
         await client.send_document(
             chat_id=LOG_CHANNEL,
             document=file_path,
             caption=caption
         )
-
-        # Delete the temporary file
         os.remove(file_path)
-
     except Exception as e:
         await message.reply_text(f"Error fetching HTML code: {e}")
 
