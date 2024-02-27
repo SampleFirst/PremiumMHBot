e80d410e082838d3e9e41bc7282266946bcca778
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-27 23:50:58 +0530

    Update bypass.py

--- plugins/functions/bypass.py
+++ plugins/functions/bypass.py
@@ -6,42 +6,36 @@ from pyrogram import Client, filters
 async def bypass_command(client, message):
     if len(message.command) == 2:
         url = message.command[1]
-        gd_link, tg_link = await extract_filepress_links(url)
+        gd_result, tg_result = await filepress(url)
         await message.reply(f"Google Drive: {gd_link}\nTelegram: {tg_link}")
     else:
         await message.reply("Invalid command. Please provide a valid URL after /bypass.")
 
 
-async def extract_filepress_links(url):
-    gd_link = ""
-    tg_link = ""
-    cget = create_scraper().request
-    try:
-        url = cget('GET', link).url
-        raw = urlparse(url)
+async def filepress(url):
+	cget = create_scraper().request
+	try:
+		url = cget('GET', url).url
+		raw = urlparse(url)
 
-        gd_data = {
-            'id': raw.path.split('/')[-1],
-            'method': 'publicDownlaod',
-        }
-        tg_data = {
-            'id': raw.path.split('/')[-1],
-            'method': 'telegramDownload',
-        }
-
-        api = f'{raw.scheme}://{raw.hostname}/api/file/downlaod/'
-
-        gd_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=gd_data).json()
-        tg_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=tg_data).json()
-
-        gd_result = f'https://drive.google.com/uc?id={gd_res["data"]}' if 'data' in gd_res else f'ERROR: {gd_res["statusText"]}'
-        tg_result = f'https://tghub.xyz/?start={tg_res["data"]}' if 'data' in tg_res else "No Telegram file available "
-
-        gd_link += gd_result + "\n"
-        tg_link += tg_result + "\n"
-
-    except Exception as e:
-        gd_link += f'Google Drive: ERROR: {e.__class__.__name__}\n'
-        tg_link += f'Telegram: ERROR: {e.__class__.__name__}\n'
-
-    return gd_link, tg_link
+		gd_data = {
+			'id': raw.path.split('/')[-1],
+			'method': 'publicDownlaod',
+		}
+		tg_data = {
+			'id': raw.path.split('/')[-1],
+			'method': 'telegramDownload',
+		}
+		
+		api = f'{raw.scheme}://{raw.hostname}/api/file/downlaod/'
+		
+		gd_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=gd_data).json()
+		tg_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=tg_data).json()
+		
+	except Exception as e:
+		return f'Google Drive: ERROR: {e.__class__.__name__} \nTelegram: ERROR: {e.__class__.__name__}'
+	
+	gd_result = f'https://drive.google.com/uc?id={gd_res["data"]}' if 'data' in gd_res else f'ERROR: {gd_res["statusText"]}'
+	tg_result = f'https://tghub.xyz/?start={tg_res["data"]}' if 'data' in tg_res else "No Telegram file available "
+	
+	return gd_result, tg_result
