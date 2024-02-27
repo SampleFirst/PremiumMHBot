f69dd4265dbbae968a11847e287b4f78bc81918e
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-27 23:44:05 +0530

    Update bypass.py

--- plugins/functions/bypass.py
+++ plugins/functions/bypass.py
@@ -16,32 +16,32 @@ async def extract_filepress_links(url):
     gd_link = ""
     tg_link = ""
     cget = create_scraper().request
-    for link in filepress_links:
-        try:
-            url = cget('GET', link).url
-            raw = urlparse(url)
+    try:
+        url = cget('GET', link).url
+        raw = urlparse(url)
 
-            gd_data = {
-                'id': raw.path.split('/')[-1],
-                'method': 'publicDownlaod',
-            }
-            tg_data = {
-                'id': raw.path.split('/')[-1],
-                'method': 'telegramDownload',
-            }
+        gd_data = {
+            'id': raw.path.split('/')[-1],
+            'method': 'publicDownlaod',
+        }
+        tg_data = {
+            'id': raw.path.split('/')[-1],
+            'method': 'telegramDownload',
+        }
 
-            api = f'{raw.scheme}://{raw.hostname}/api/file/downlaod/'
+        api = f'{raw.scheme}://{raw.hostname}/api/file/downlaod/'
 
-            gd_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=gd_data).json()
-            tg_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=tg_data).json()
+        gd_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=gd_data).json()
+        tg_res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=tg_data).json()
 
-            gd_result = f'https://drive.google.com/uc?id={gd_res["data"]}' if 'data' in gd_res else f'ERROR: {gd_res["statusText"]}'
-            tg_result = f'https://tghub.xyz/?start={tg_res["data"]}' if 'data' in tg_res else "No Telegram file available "
+        gd_result = f'https://drive.google.com/uc?id={gd_res["data"]}' if 'data' in gd_res else f'ERROR: {gd_res["statusText"]}'
+        tg_result = f'https://tghub.xyz/?start={tg_res["data"]}' if 'data' in tg_res else "No Telegram file available "
 
-            gd_link += gd_result + "\n"
-            tg_link += tg_result + "\n"
+        gd_link += gd_result + "\n"
+        tg_link += tg_result + "\n"
 
-        except Exception as e:
-            gd_link += f'Google Drive: ERROR: {e.__class__.__name__}\n'
-            tg_link += f'Telegram: ERROR: {e.__class__.__name__}\n'
+    except Exception as e:
+        gd_link += f'Google Drive: ERROR: {e.__class__.__name__}\n'
+        tg_link += f'Telegram: ERROR: {e.__class__.__name__}\n'
+
     return gd_link, tg_link
