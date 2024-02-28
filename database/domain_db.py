a7ac15341034621b644adb908deec7d9df023d47
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-02-28 21:52:44 +0530

    Update domain_db.py

--- database/domain_db.py
+++ database/domain_db.py
@@ -11,26 +11,27 @@ class Database:
         self.dm = self._client[database_name]
         self.dm = self.dm.domain
 
-    async def add_domain(self, domain):
+    async def add_domain(self, site, domain):
         tz = pytz.timezone('Asia/Kolkata')
         current_time = datetime.now(tz)
         domain_data = {
+            'site': site,
             'domain': domain,
             'timestamp': current_time
         }
         await self.dm.insert_one(domain_data)
 
-    async def get_latest_domain(self):
-        latest_domain = await self.dm.find_one({}, sort=[('timestamp', -1)])
+    async def get_latest_domain(self, site):
+        latest_domain = await self.dm.find_one({'site': site}, sort=[('timestamp', -1)])
         return latest_domain['domain'] if latest_domain else None
 
-    async def get_all_domains(self):
+    async def get_all_domains(self, site):
         all_domains = []
-        async for domain_data in self.dm.find({}, {'_id': 0}):
+        async for domain_data in self.dm.find({'site': site}, {'_id': 0}):
             all_domains.append(domain_data)
         return all_domains
     
-    async def delete_all_domains(self):
-        await self.dm.delete_many({})
+    async def delete_all_domains(self, site):
+        await self.dm.delete_many({'site': site})
 
 dm = Database(DATABASE_URI, DATABASE_NAME)
