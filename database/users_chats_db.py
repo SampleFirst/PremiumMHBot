724e855a28ee1ff4af1d4a642dfdcecb2bbb1995
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-03 00:09:51 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -4,34 +4,46 @@ from info import DATABASE_NAME, DATABASE_URI
 
 
 class Database:
-    
+
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
 
-
     def new_user(self, id, name):
         return dict(
-            id = id,
-            name = name,
+            id=id,
+            name=name,
+            file_id=False,
             ban_status=dict(
                 is_banned=False,
                 ban_reason="",
+            ),
+            bot_status=dict(
+                bot_name=None,
+                now_status=None,
+                now_date=None,
+                expiry_date=None,
+            ),
+            db_status=dict(
+                db_name=None,
+                now_status=None,
+                now_date=None,
+                expiry_date=None,
             ),
         )
 
     def new_group(self, id, title):
         return dict(
-            id = id,
-            title = title,
+            id=id,
+            title=title,
             chat_status=dict(
                 is_disabled=False,
                 reason="",
             ),
         )
-    
+
     async def update_verification(self, id, date, time):
         status = {
             'date': str(date),
@@ -48,26 +60,26 @@ class Database:
         if user:
             return user.get("verification_status", default)
         return default
-    
+
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
-    
+
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
+        user = await self.col.find_one({'id': int(id)})
         return bool(user)
-    
+
     async def total_users_count(self):
         count = await self.col.count_documents({})
         return count
-    
+
     async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
             ban_reason=''
         )
         await self.col.update_one({'id': id}, {'$set': {'ban_status': ban_status}})
-    
+
     async def ban_user(self, user_id, ban_reason="No Reason"):
         ban_status = dict(
             is_banned=True,
@@ -80,13 +92,13 @@ class Database:
             is_banned=False,
             ban_reason=''
         )
-        user = await self.col.find_one({'id':int(id)})
+        user = await self.col.find_one({'id': int(id)})
         if not user:
             return default
         return user.get('ban_status', default)
 
     async def get_all_users(self):
-        return self.col.find({})    
+        return self.col.find({})
 
     async def delete_user(self, user_id):
         await self.col.delete_many({'id': int(user_id)})
@@ -97,39 +109,84 @@ class Database:
         b_chats = [chat['id'] async for chat in chats]
         b_users = [user['id'] async for user in users]
         return b_users, b_chats
-    
+
     async def add_chat(self, chat, title):
         chat = self.new_group(chat, title)
         await self.grp.insert_one(chat)
-    
+
     async def get_chat(self, chat):
-        chat = await self.grp.find_one({'id':int(chat)})
+        chat = await self.grp.find_one({'id': int(chat)})
         return False if not chat else chat.get('chat_status')
-    
+
     async def re_enable_chat(self, id):
-        chat_status=dict(
+        chat_status = dict(
             is_disabled=False,
             reason="",
         )
         await self.grp.update_one({'id': int(id)}, {'$set': {'chat_status': chat_status}})
-        
+
     async def disable_chat(self, chat, reason="No Reason"):
-        chat_status=dict(
+        chat_status = dict(
             is_disabled=True,
             reason=reason,
         )
         await self.grp.update_one({'id': int(chat)}, {'$set': {'chat_status': chat_status}})
-    
+
     async def total_chat_count(self):
         count = await self.grp.count_documents({})
         return count
-    
+
     async def get_all_chats(self):
         return self.grp.find({})
+
+    async def update_status_bot(self, user_id, bot_name, now_status, now_date, expiry_date):
+        user_status = dict(
+            bot_name=bot_name,
+            now_status=now_status,
+            now_date=now_date,
+            expiry_date=expiry_date,
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'user_status': user_status}})
+
+    async def update_status_db(self, user_id, db_name, now_status, now_date, expiry_date):
+        user_status = dict(
+            db_name=db_name,
+            now_status=now_status,
+            now_date=now_date,
+            expiry_date=expiry_date,
+        )
+        await self.col.update_one({'id': user_id}, {'$set': {'user_status': user_status}})
+
+    async def is_status_exist_bot(self, user_id, bot_name, now_status):
+        user = await self.col.find_one({'id': user_id, 'user_status.bot_name': bot_name, 'user_status.now_status': now_status})
+        return bool(user)
+
+    async def is_status_exist_db(self, user_id, db_name, now_status):
+        user = await self.col.find_one({'id': user_id, 'user_status.db_name': db_name, 'user_status.now_status': now_status})
+        return bool(user)
+
+    async def total_status_bot(self, bot_name=None, now_status=None):
+        query = {}
+        if bot_name:
+            query['user_status.bot_name'] = bot_name
+        if now_status:
+            query['user_status.now_status'] = now_status
+
+        count = await self.col.count_documents(query)
+        return count
+
+    async def total_status_db(self, db_name=None, now_status=None):
+        query = {}
+        if db_name:
+            query['user_status.db_name'] = db_name
+        if now_status:
+            query['user_status.now_status'] = now_status
+
+        count = await self.col.count_documents(query)
+        return count
 
     async def get_db_size(self):
         return (await self.db.command("dbstats"))['dataSize']
 
 
 db = Database(DATABASE_URI, DATABASE_NAME)
-
