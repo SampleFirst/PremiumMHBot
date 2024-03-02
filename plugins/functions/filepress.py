a1fed0a69108401c98a8d15f1de8688d1a5a522c
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-02 14:52:47 +0530

    Update filepress.py

--- plugins/functions/filepress.py
+++ plugins/functions/filepress.py
@@ -44,6 +44,6 @@ async def filepress_command(client: Client, message: Message):
     url = message.command[1]
     try:
         result = await filepress(url)
-        await message.reply_text(result, parse_mode='html')
+        await message.reply_text(result)
     except Exception as e:
         await message.reply_text(f"Error: {e}")
