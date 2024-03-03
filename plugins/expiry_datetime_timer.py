8eef035b7a7531f85f121d500c6b06f6c4a2c06f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-03 17:37:38 +0530

    Update expiry_datetime_timer.py

--- plugins/expiry_datetime_timer.py
+++ plugins/expiry_datetime_timer.py
@@ -1,9 +1,9 @@
 # expiry_datetime_timer.py
 import logging
 import asyncio
+import datetime
 from info import ADMINS, LOG_CHANNEL 
 from database.users_chats_db import db
-from Script import script
 from plugins.datetime import get_datetime 
 from plugins.expiry_datetime import get_expiry_datetime, get_expiry_name
 from plugins.get_name import get_bot_name, get_db_name
@@ -12,9 +12,16 @@ from plugins.status_name import get_status_name
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-async def add_expiry_date_timer(user_id, expiry_date):
+async def add_expiry_date_timer(client, user_id, bot_name, expiry_date):
     current_time = datetime.datetime.now()
     time_difference = expiry_date - current_time
     await asyncio.sleep(time_difference.total_seconds())
-    await db.update_status_bot(user_id, bot_name, now_status, expiry_date, expiry_date)
-    await client.send_message(user_id, "Your premium status for the bot has expired.")
+    
+    user_id = user_id
+    bot_name = bot_name
+    now_status = get_status_name(status_num=7)
+    now_date = get_datetime(format_type=19)
+    
+    await db.update_status_bot(user_id, bot_name, now_status, now_date, None)
+    await client.send_message(user_id, f"Your attempt status for the {bot_name} has expired.")
+    await client.send_message(LOG_CHANNEL, f"{user_id} attempt status for the {bot_name} has expired. and new Status {now_status}")
