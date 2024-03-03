2e662ca75440ec35033e3b0ccf0cef4eb3e080ae
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-03 14:30:52 +0530

    Update filepress.py

--- plugins/functions/filepress.py
+++ plugins/functions/filepress.py
@@ -1,27 +1,26 @@
-from pyrogram import Client, filters, enums
+from pyrogram import Client, filters
 from pyrogram.types import Message
 import aiohttp
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from cloudscraper import create_scraper
 
-
 async def filepress(url: str):
-    async with aiohttp.ClientSession() as sess:
+    async with aiohttp.ClientSession() as session:
         scraper = create_scraper()
         try:
             url = scraper.get(url).url
             raw = urlparse(url)
             json_data = {
                 'id': raw.path.split('/')[-1],
-                'method': 'publicDownlaod',
+                'method': 'publicDownload',
             }
-            async with sess.post(f'{raw.scheme}://{raw.hostname}/api/file/telegram/downlaod/', headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=json_data) as resp:
-                tg_id = await resp.json()
+            async with session.post(f'{raw.scheme}://{raw.hostname}/api/file/telegram/download/', headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=json_data) as response:
+                tg_id = await response.json(content_type="text/html")
             if 'data' in tg_id:
                 tg_id_data = tg_id['data']
                 tg_url = f"https://tghub.xyz/?start={tg_id_data}"
-                bot_name = [bot for bot in BeautifulSoup(await sess.get(tg_url)).text if "filepress_[a-zA-Z0-9]+_bot" in bot][0]
+                bot_name = [bot for bot in BeautifulSoup(await session.get(tg_url).text) if "filepress_[a-zA-Z0-9]+_bot" in bot][0]
                 tg_link = f"https://t.me/{bot_name}/?start={tg_id_data}"
             else:
                 tg_link = 'Unavailable' if tg_id["statusText"] == "Ok" else tg_id["statusText"]
@@ -36,8 +35,9 @@ async def filepress(url: str):
     return parse_txt
 
 
+
 @Client.on_message(filters.command("filepress") & filters.private)
-async def filepress_command(client: Client, message: Message):
+async def filepress_extracter(client: Client, message: Message):
     if len(message.command) != 2:
         await message.reply_text("Usage: /filepress url")
         return
@@ -47,5 +47,4 @@ async def filepress_command(client: Client, message: Message):
         await message.reply_text(result)
     except Exception as e:
         await message.reply_text(f"Error: {e}")
- 
-               
+
